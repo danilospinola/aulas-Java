8b32c82df287090e24039def08990fc3efbc3375
aluno <aluno@DSN1251230722>
2022-04-04 10:38:37 -0300

    s

---
+++ OrientacaoObjeto/src/Calculadora.java
@@ -0,0 +1,21 @@
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+ double n1 = 10;
+ double n2  = 2;
+ double resulso;
+ double resulsu;
+ double resuldiv;
+ double resulmut;
+	
+ resulso = n1 + n2;
+ 	System.out.println("Resultado da soma é : "+resulso);
+ resulsu = n1 - n2;
+ 	System.out.println("Resultado da subtração é : "+resulsu);
+ resuldiv = n1 / n2;
+ 	System.out.println("Resultado da divisão é : "+resuldiv);
+ resulmut = n1 * n2;
+ 	System.out.println("Resultado da mutiplicação é : "+resulmut);
+	}
+}	
