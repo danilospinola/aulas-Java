e1730b1268373123e8e1718e31e57a2c68d74292
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-04-14 11:22:09 -0300

    Add files via upload
    2°commit

--- PIX/src/Pessoa.java
+++ PIX/src/Pessoa.java
@@ -1,14 +1,28 @@
 public class Pessoa {
-	
+
 	double valor;
 	int cpf;
 	String nome;
 	int chavepix;
-	
-	void transferir (int valor) {
-		this.valor += valor;
+	double total;
+
+	boolean transferir(double valor, Pessoa pessoa) {
+		if (pessoa.valor <  pessoa.total) {
+			System.out.println("saldo insuficiente");
+			return false;
+		} else {
+			this.total -= valor;
+			System.out.println(nome + " transferiu " + valor + "R$");
+			System.out.println(nome + " seu saldo total é de " + total);
+			pessoa.receber(valor);
+			return true;
+		}
+
 	}
-	void receber (int valor) {
-		this.valor += valor;
-}
+
+	void receber(double valor) {
+		this.total += valor;
+		System.out.println(nome + " recebe " + valor + "R$");
+		System.out.println(nome +" seu saldo total é de " + total);
+	}
 }
