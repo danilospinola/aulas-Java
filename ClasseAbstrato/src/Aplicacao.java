e968d2bec31da92f348af76a2a1c2cc00cb0497e
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-05-09 11:39:57 -0300

    Add files via upload
    primeiro commit

---
+++ ClasseAbstrato/src/Aplicacao.java
@@ -0,0 +1,12 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Cogumelo cogumelo = new Cogumelo ();
+		
+		cogumelo.pegar();
+	}
+
+}
---
+++ ClasseAbstrato/src/Item.java
@@ -0,0 +1,22 @@
+
+public abstract class Item {
+	
+	private int posx;
+	private int posy;
+	
+	public abstract void pegar();
+	
+	//getter e setter
+	public int getPosx() {
+		return posx;
+	}
+	public void setPosx(int posx) {
+		this.posx = posx;
+	}
+	public int getPosy() {
+		return posy;
+	}
+	public void setPosy(int posy) {
+		this.posy = posy;
+	}
+}
