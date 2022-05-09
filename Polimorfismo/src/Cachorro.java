a0be6b203c7a8664f3d14f9053296625e0cbc0d4
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-05-09 09:36:05 -0300

    Add files via upload

---
+++ Polimorfismo/src/Cachorro.java
@@ -0,0 +1,15 @@
+
+public class Cachorro extends Animal{
+
+
+
+	@Override
+	public void Falar() {
+		// TODO Auto-generated method stub
+		System.out.println("AUAU");
+	}
+
+	public void Morder() {
+		System.out.println("NHAC ");
+	}
+}
