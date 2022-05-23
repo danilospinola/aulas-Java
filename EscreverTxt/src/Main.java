8d4f4eb151ef01b5fd9dc54125042d5f1cf018e9
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-05-23 11:41:42 -0300

    Add files via upload

---
+++ EscreverTxt/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+	 String s = "texto para ser gravado no arquivo.";
+	
+    try (BufferedWriter escrever =  new BufferedWriter(new FileWriter("saida.txt"))){
+	
+	escrever.write(s);
+    }
+	
+	}
+
+}
