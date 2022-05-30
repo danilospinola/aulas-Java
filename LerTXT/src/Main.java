daebbe803f6f0a5b0a617a943895dd05a6526083
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-05-30 11:36:47 -0300

    Add files via upload

--- LerTXT/src/Main.java
+++ LerTXT/src/Main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -11,18 +13,35 @@ public class Main {
 			String string = "";
 
 			while ((line = reader.readLine()) != null) {
-				string += line + "\n";
+				string+= line + "\n";
 			}
 			System.out.println(string);
 		}
+
+		String classe = "Mouse: funciona = true, qtdBotoes= 5, tamanho = pequeno\n";
+
+		Mouse mouse1 = new Mouse(classe);
+
 		
-		String classe = "Mouse: funciona = sim, qtdBotoes= 5, tamanho = pequeno\n";
 		
-		Mouse mouse = new Mouse(classe);
+		classe = "Mouse: funciona = true, qtdBotoes= 5, tamanho = medio\n";
+
+		Mouse mouse2 = new Mouse(classe);
+
 		
-		System.out.println(mouse);
+		
+		classe = "Mouse: funciona = true, qtdBotoes= 5, tamanho = Grande\n";
+
+		Mouse mouse3 = new Mouse(classe);
+
+		List<Mouse> listaMouse = new ArrayList<Mouse>();
+		listaMouse.add(mouse1);
+		listaMouse.add(mouse2);
+		listaMouse.add(mouse3);
+
+		for (Mouse mouse : listaMouse) {
+			System.out.println(mouse);
+		}
 	}
 
-	
-	
 }
