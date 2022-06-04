86b9fce1832be9222d52a579728f896b7f6ca95f
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-06-04 19:56:37 -0300

    Update Endereco.java

--- Cadastro/src/Endereco.java
+++ Cadastro/src/Endereco.java
@@ -9,16 +9,17 @@ public class Endereco {
 	
 	public Endereco(String[] ArrayLine) {
 		
-		String[] ArrayRua = ArrayLine[3].split("=");
-		this.rua = ArrayRua[1].trim();
+		String[] ArrayRua = ArrayLine[3].split("=");  //Quebra a variavel Line já partida no pessoa, e a partir da terceira parte de um "=" a divide em dois, além disso coloca na variavel ArrayRua
+		this.rua = ArrayRua[1].trim(); //pega a 1° parte da divisão (realizada anterior) sendo que tem a 0 e a 1 e remove espacos do string e quarda no atributo rua.
 				
-		String[] ArrayNumero = ArrayLine[4].split("=");
-		this.numero = Integer.parseInt(ArrayNumero[1].trim());
+		String[] ArrayNumero = ArrayLine[4].split("="); //Quebra novamente a variavel Line e coloca e coloca a string na variavel ArrayNumero.
+		this.numero = Integer.parseInt(ArrayNumero[1].trim()); //Pega a primeira parte do ArrayNumero e coloca no atributo numero 
 		
-		String[] ArrayBairro = ArrayLine[5].split("=");
-		this.bairro = ArrayBairro[1].trim();
+		String[] ArrayBairro = ArrayLine[5].split("="); // mesma processo dos anteriores porém coloca na variavel ArrayBairro
+		this.bairro = ArrayBairro[1].trim(); ///Pega a primeira parte do ArrayBairro e coloca no atributo numero 
 	}
 
+	//<---------------------------------------- Getters e Setters --------------------------------------->
 	public Endereco() {
 		
 	}
@@ -53,10 +54,12 @@ public class Endereco {
 		this.bairro = bairro;
 	}
 
+	
+	//<---------------------------------------- Getters e Setters --------------------------------------->
 
 	@Override
 	public String toString() {
-		return ", Rua=" + rua + ", Numero=" + numero + ", Bairro=" + bairro ;
+		return ", Rua=" + rua + ", Numero=" + numero + ", Bairro=" + bairro ; //Override do to string
 	}
 	
 	
