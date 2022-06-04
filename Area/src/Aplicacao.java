709619c4301a344d093325e405c0e54c85670a74
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-06-04 15:04:29 -0300

    Update Aplicacao.java

--- Area/src/Aplicacao.java
+++ Area/src/Aplicacao.java
@@ -4,18 +4,23 @@ public class Aplicacao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Quadrado quadrado = new Quadrado();
-		Retangulo retangulo = new Retangulo();
-		Circulo circulo = new Circulo();
-		quadrado.setLado(30);
-		quadrado.calcularArea();
+		Quadrado quadrado = new Quadrado(); //Instância um quadrado
+		Retangulo retangulo = new Retangulo();  //Instância um Retangulo
+		Circulo circulo = new Circulo();  //Instância um circulo 
+		
+		quadrado.setLado(30); //Coloca o lado do quadrado como 30
+		quadrado.calcularArea(); // usa o metodo calcular área do quadrado
+		
 		System.out.println("------------------------------------------------------------------");
-		retangulo.setAltura(40);
-		retangulo.setBase(70);
-		retangulo.calcularArea();
+		
+		retangulo.setAltura(40); //Coloca a altura do retangulo como 40
+		retangulo.setBase(70); // Coloca a base do retangulo como 70
+		retangulo.calcularArea(); // usa o metodo calcular área do retangulo
+		
 		System.out.println("------------------------------------------------------------------");
-		circulo.setRaio(15);
-		circulo.calcularArea();
+		
+		circulo.setRaio(15); // Coloca o raio do circulo como 15
+		circulo.calcularArea(); //Calcula a área do 
 		
 	}
 
