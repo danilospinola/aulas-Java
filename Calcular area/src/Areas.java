238ae67137bd3d77b705882235c181a67874e396
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-06-04 23:51:27 -0300

    Update Areas.java

--- Calcular area/src/Areas.java
+++ Calcular area/src/Areas.java
@@ -1,17 +1,17 @@
 
 public class Areas {
 
-	int calculararea(int x, int y) {
+	int calculararea(int x, int y) { // Pega 2 numeros e calcula a área de um triangulo
 
 		return (x * y) / 2;
 	}
 
-	double calculararea(double x, double y) {
+	double calculararea(double x, double y) { // Pega 2 numeros divide por 2 e calcula a área de um retangulo
 
 		return (x * y);
 	}
 
-	int calculararea(int x) {
+	int calculararea(int x) { //pega um numero e calcula a área de um quadrado
 
 		return (x * x);
 	}
