04d339b0e502e7212e105dcf6f09ea4cb0df56ad
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-06-05 01:15:35 -0300

    Update Matemática.java

--- ElementosEstaticos/src/Matemática.java
+++ ElementosEstaticos/src/Matemática.java
@@ -1,11 +1,11 @@
 
-public class Matemática {
+public class MatemÃ¡tica {
 
-	public static int somar(int a, int b) {
+	public static int somar(int a, int b) { // Soma a dois numeros
 		return a +b;
 	}
 	
-	public static int subtrair(int a, int b) {
+	public static int subtrair(int a, int b) { // subtrai a dois numeros
 		return a-b;                                                                                   
 	}
 }
