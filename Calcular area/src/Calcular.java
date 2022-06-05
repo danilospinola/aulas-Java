aee2e28b6cbddf099970efa9c24c2cc34f01cd4b
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-06-05 00:08:10 -0300

    Update Calcular.java

--- Calcular area/src/Calcular.java
+++ Calcular area/src/Calcular.java
@@ -4,25 +4,25 @@ public class Calcular {
 
 	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
-		Areas areas = new Areas();
+		Scanner scanner = new Scanner(System.in); //instancia um scanner
+		Areas areas = new Areas(); //Instancia uma area
 
-		System.out.println("digite a base do poligono para calcular a área: ");
-		int x = scanner.nextInt();
+		System.out.println("digite a base do poligono para calcular a Ã¡rea: ");  
+		int x = scanner.nextInt(); // Valor do X e igual ao que e digitado no teclado
 
-		int Quadrado = areas.calculararea(x);
-		System.out.println("resultado do quadrado é igual a : " + Quadrado);
+		int Quadrado = areas.calculararea(x); // Usa a variavel x para o metodo calculararea e guarda na variavel Quadrado.
+		System.out.println("resultado do quadrado Ã© igual a : " + Quadrado);
 
-		System.out.println("digite a altura para calcular o  triangulo");
-		int y = scanner.nextInt();
+		System.out.println("digite a altura para calcular o  triangulo"); 
+		int y = scanner.nextInt(); // Valor de Y e igual ao que e digitado no teclado
 
-		int triangulo = areas.calculararea(x, y);
+		int triangulo = areas.calculararea(x, y); // Usa a variavel x e Y para o metodo calculararea e guarda na variavel Triangulo.
 		System.out.println("reultado do retangulo igual a : " + triangulo);
 
-		System.out.println("digite a altura para calcular o retangulo");
+		System.out.println("digite a altura para calcular o retangulo"); // Usa a variavel x e Y para o metodo calculararea e guarda na variavel Triangulo.
 		double y2 = scanner.nextDouble();
-		double retangulo = areas.calculararea(x, y2);
-		System.out.println("reultado é igual a : " + retangulo);
+		double retangulo = areas.calculararea(x, y2); 
+		System.out.println("reultado Ã© igual a : " + retangulo);
 	}
 
 }
