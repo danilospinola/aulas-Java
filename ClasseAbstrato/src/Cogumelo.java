5fdf7664eaab8efa03186919e2fa977b7cdc91bf
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-06-05 00:41:13 -0300

    Update Cogumelo.java

--- ClasseAbstrato/src/Cogumelo.java
+++ ClasseAbstrato/src/Cogumelo.java
@@ -2,7 +2,7 @@
 public class Cogumelo extends Item{
 
 	@Override
-	public void pegar() {	// Faz override do metodo pegar 
+	public void pegar() {	// Faz override do metodo obrigatorio pegar 
 		System.out.println("Pegar cogumelo");
 	}
 
