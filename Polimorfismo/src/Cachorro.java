5518daa7464332d91cc4e5491f7de791eeb4136d
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-06-06 10:16:35 -0300

    Update Cachorro.java

--- Polimorfismo/src/Cachorro.java
+++ Polimorfismo/src/Cachorro.java
@@ -3,13 +3,13 @@ public class Cachorro extends Animal{
 
 
 
-	@Override
+	@Override //Override do método falar da super classe animal.
 	public void Falar() {
 		// TODO Auto-generated method stub
-		System.out.println("AUAU");
+		System.out.println("AUAU"); ;//Mostra no console "AUAU"
 	}
 
-	public void Morder() {
-		System.out.println("NHAC ");
+	public void Morder() { //Método morder proprio de cachorro
+		System.out.println("NHAC ");  //Mostra no console "Nhac"
 	}
 }
