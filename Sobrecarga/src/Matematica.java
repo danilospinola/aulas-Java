09c6dd9bd87a5f9aa77f27a0daef131d68479a4f
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-06-06 10:29:45 -0300

    Update Matematica.java

--- Sobrecarga/src/Matematica.java
+++ Sobrecarga/src/Matematica.java
@@ -1,15 +1,18 @@
 
 public class Matematica {
-	int somar (int x, int y){
+	int somar (int x, int y){ //Método que soma 2 numeros e os retorna no console, com o nome somar.
 		 
 		 return x + y;
  }
-	int somar (int x, int y, int z){
+	
+	
+	int somar (int x, int y, int z){ //Método que soma 3 numeros  e os retorna no console com o nome somar, mostrando assim a sobrecarga de métodos(Métodos do mesmo nome porém com "requisições diferentes").
 		
-	return	x + y + z;
+	return	x + y + z; 
 	}
 	
-	double somar (double x , double y) {
+	
+	double somar (double x , double y) { // Método com o nome somar, porém soma dois double  e os retorna no console.
 		
 		return x + y;
 	}
