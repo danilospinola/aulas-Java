b17d8e3c5648f447e80d8a9cebee715eb935787a
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-06-06 10:14:23 -0300

    Update main.java

--- OrientacaoObjeto/src/main.java
+++ OrientacaoObjeto/src/main.java
@@ -5,14 +5,14 @@ public class main {
 
 		Pessoa pessoa1 = new Pessoa();
 		pessoa1.nome = "Danilo";
-		pessoa1.numfigurinhas = 1;
+		pessoa1.numfigurinhas = 1; //Inicia danilo com 1 figurinha
 		
-		pessoa1.receber(3);
-		pessoa1.dar(3);
+		pessoa1.receber(3); //Usa o metodo receber figurinhas de pessoa
+		pessoa1.dar(3); //Usa o metodo dar figurinhas de pessoa
 		
-		System.out.println(pessoa1.nome);
-		System.out.println(pessoa1.numfigurinhas);
-		
+		System.out.println(pessoa1.nome); //Mostra no console o nome da pessoa
+		System.out.println(pessoa1.numfigurinhas);  //Mostra no console o numero de figurinhas da pessoa
+		 
 	}
 
 }
