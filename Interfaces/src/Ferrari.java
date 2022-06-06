f5b27c8e2113adea6fa26a549a4429af9b9512b1
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-06-06 08:52:57 -0300

    Update Ferrari.java

--- Interfaces/src/Ferrari.java
+++ Interfaces/src/Ferrari.java
@@ -3,36 +3,31 @@ public class Ferrari implements Carro, ItemCaro{
 //Classe Ferrari 
 	
 	@Override
-	public void virarEsquerda() {
-		// TODO Auto-generated method stub
+	public void virarEsquerda() { //Override do método virar a esquerda da interface Veiculo, que tambem contem em veiculo.
 		System.out.println("Ferrari virou a esquerda");
 	}
 
 	@Override
-	public void virarDireita() {
-		// TODO Auto-generated method stub
+	public void virarDireita() { //Override do método virar a direita da interface Veiculo, que tambem contem em veiculo.
 		System.out.println("Ferrari virou a direita");
 	}
 
 	@Override
-	public void acelerar() {
-		// TODO Auto-generated method stub
+	public void acelerar() { //Override do método acelerar da interface Veiculo, que tambem contem em veiculo.
 		System.out.println("Ferrari acelerou");
 	}
 
 	// Metodo da interface Carro
 	
-	@Override
-	public void AbrirPorta() {
-		// TODO Auto-generated method stub
+	@Override		//Override do método abrir porta da interface Carro.
+	public void AbrirPorta() { 
 		System.out.println("Ferrari abriu a porta");
 	}
 
 	// Metodo da interface Item caro
 	
 	@Override
-	public double getPreco() {
-		// TODO Auto-generated method stub
+	public double getPreco() { //Override do método getPreco da interface Item caro.
 		return 500000;
 	}
 
