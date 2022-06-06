be39995e66a17b1a26fa63129be866d5d0e100e0
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-06-06 08:45:10 -0300

    Update Automovel.java

--- Interfaces/src/Automovel.java
+++ Interfaces/src/Automovel.java
@@ -1,7 +1,7 @@
 
 public interface Automovel {
 
-	void virarEsquerda(); 
+	void virarEsquerda();  //Interface automovel que possui métodos de virar a esquerda, virar a direita e acelerar
 	void virarDireita();
 	void acelerar();
 }
