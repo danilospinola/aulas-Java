a9f48c756280c13f79d78d91bdd2fbfdec4101e2
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-06-06 08:58:03 -0300

    Update Rota.java

--- Interfaces/src/Rota.java
+++ Interfaces/src/Rota.java
@@ -1,10 +1,10 @@
 
 public class Rota {
 
-	public void ir(Automovel automovel) {
-		automovel.virarDireita();
-		automovel.acelerar();
-		automovel.virarEsquerda();
-		automovel.virarDireita();
+	public void ir(Automovel automovel) { // Requer um automovel para realizar o método.
+		automovel.virarDireita(); // Realiza o método virar a direita do automovel
+		automovel.acelerar(); // Realiza o método acelerar do automovel
+		automovel.virarEsquerda(); // Realiza o método virar a esquerda do automovel
+		automovel.virarDireita(); // Realiza o método virar a direita do automovel
 	}
 }
