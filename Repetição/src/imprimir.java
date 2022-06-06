db5c1a06f044e0f65889c6d5968ab392e5b7bc45
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-06-06 10:26:45 -0300

    Update imprimir.java

--- Repetição/src/imprimir.java
+++ Repetição/src/imprimir.java
@@ -3,8 +3,8 @@ public class imprimir {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int i = 10; i < 26; i++) {
-			  System.out.println(i);
+		for (int i = 10; i < 26; i++) { //for que faz com que toda vez que a variavel "i" do tipo inteiro que começa em 10, for menor que 26 adiciona mais 1 a "i" e o imprimi no console 
+			  System.out.println(i); 
 		
 			  
 	
