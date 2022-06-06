65ff4d98d7236fd616f86bda3893c424f7880f1a
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-06-06 08:55:23 -0300

    Update Fusca.java

--- Interfaces/src/Fusca.java
+++ Interfaces/src/Fusca.java
@@ -1,27 +1,28 @@
 
 public class Fusca implements Carro {
-	//Classe Fusca
+	//Classe Fusca que contem métodos da interface carro
 	
-	//Metodo da interface Automovel
 	
+	
+	//Override do Metodo virar a esquerda da interface Automovel que existe támbem na interface carro
 	@Override
 	public void virarEsquerda() {
 		// TODO Auto-generated method stub
 		System.out.println("Fusca virou a esquerda");
 	}
-
+	//Override do Metodo virar a direita da interface Automovel que existe támbem na interface carro
 	@Override
 	public void virarDireita() {
 		// TODO Auto-generated method stub
 		System.out.println("Fusca virou a direita");
 	}
-
+	//Override do Metodo acelerar da interface Automovel que existe támbem na interface carro
 	@Override
 	public void acelerar() {
 		// TODO Auto-generated method stub
 		System.out.println("Fusca acelerou");
 	}
-
+	//Override do Metodo Abrir porta da interface carro
 	@Override
 	public void AbrirPorta() {
 		// TODO Auto-generated method stub
