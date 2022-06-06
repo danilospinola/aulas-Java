bfee0726c4db8aa573c58ac7f133c973d279b63b
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-06-06 08:59:25 -0300

    Update Apartamento.java

--- Interfaces/src/Apartamento.java
+++ Interfaces/src/Apartamento.java
@@ -1,8 +1,8 @@
 
-public class Apartamento implements ItemCaro{
+public class Apartamento implements ItemCaro{ // Mostra que apartamento possui métodos da interface item caro
 
 	@Override
-	public double getPreco() {
+	public double getPreco() { // Faz override do método getpreco da interface
 		// TODO Auto-generated method stub
 		return 250000;
 	}
