81d7adc24776d4948b46fc67c463ee8fecdd0846
danilospinola <102962780+danilospinola@users.noreply.github.com>
2022-06-06 09:59:41 -0300

    Update Cliente.java

--- ModificadorAcesso/src/Cliente.java
+++ ModificadorAcesso/src/Cliente.java
@@ -1,51 +1,52 @@
-import java.util.Objects;
+import java.util.Objects; // Importa a casse object da biblioteca do java
 
 public class Cliente {
-
-private String nome;
-private int cpf;
-private int numConta;
+	// Atributos pivaddos do cliente, os acessos so podem ser feitos por uma subclasse ou por getters e setter.
+	private String nome;
+	private int cpf;
+	private int numConta;
 	
-
-
-	public int getCpf() {
-		return cpf;
+	// Método genérico de  Cliente
+	public Cliente() {
+		
 	}
-
-	public void setCpf(int cpf) {
-		this.cpf = cpf;
-	}
-
+	
+	// <-------------------------------------------------Getters e setters--------------------------------------------->
+	// Get pega o valor do atributo e set coloca um valor para o atributo
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
 	public int getNumConta() {
 		return numConta;
 	}
-
 	public void setNumConta(int numConta) {
 		this.numConta = numConta;
 	}
-
+	// <-------------------------------------------------Getters e setters--------------------------------------------->
+	
+	//Override do toString, método que retorna todos os atributos e mostra no console
 	@Override
 	public String toString() {
-		return  "--------------------------------------------------------------------\n"+
-				"Cliente:" +"\nnome:"+nome +"\nnumConta:" +numConta + "\ncpf:"+cpf+
-				"\n--------------------------------------------------------------------";
+		return "Nome = " + nome + "\nCPF = " + cpf + "\nNúmero de Conta = " + numConta+"\n";
 	}
 
+	//Override do hashCode, método que retorna os atributos como objetos.
 	@Override
 	public int hashCode() {
 		return Objects.hash(cpf, nome, numConta);
 	}
 
 	@Override
-	public boolean equals(Object obj) {
+	public boolean equals(Object obj) { 
 		if (this == obj)
 			return true;
 		if (obj == null)
@@ -54,7 +55,7 @@ private int numConta;
 			return false;
 		Cliente other = (Cliente) obj;
 		return cpf == other.cpf && Objects.equals(nome, other.nome) && numConta == other.numConta;
-	}	
+	}
 	
-
+	
 }
